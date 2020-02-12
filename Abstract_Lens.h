fa5b3121d7411ba470b4a3bcead8292ae8fe638c
Luke Fredrickson <lukeefredrickson@gmail.com>
2020-02-12 15:39:16 -0500

    Created additional necessary classes

---
+++ Abstract_Lens.h
@@ -0,0 +1,14 @@
+//
+// Created by Luke on 2/12/2020.
+//
+
+#ifndef LF_PROJECT2_ABSTRACT_LENS_H
+#define LF_PROJECT2_ABSTRACT_LENS_H
+
+
+class Abstract_Lens {
+
+};
+
+
+#endif //LF_PROJECT2_ABSTRACT_LENS_H
